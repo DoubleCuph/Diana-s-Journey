275b6e9068e91480ccb782c65f65570a1d6674b8
DoubleCuph <doublecuph@gmail.com>
2025-04-22 22:14:31 +0300

    Update admin.js

--- admin/admin.js
+++ admin/admin.js
@@ -4,10 +4,10 @@ document.getElementById('login-form').addEventListener('submit', function(event)
     let username = document.getElementById('username').value;
     let password = document.getElementById('password').value;
 
-    // Dati di esempio (in una versione avanzata, usa un database)
-    if (username === "Diana" && password === "trisko.10") {
-        localStorage.setItem('adminLoggedIn', 'true');
-        window.location.href = "admin-panel.html";
+    // Dati di accesso base (da migliorare con database per sicurezza)
+    if (username === "admin" && password === "password123") {
+        localStorage.setItem('adminLoggedIn', 'true'); // Memorizza lo stato di login
+        window.location.href = "admin-panel.html"; // Reindirizza al pannello amministrativo
     } else {
         alert("Credenziali errate!");
     }
