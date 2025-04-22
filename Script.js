b980737890927ef59c035a31ae1916f001a9d56c
DoubleCuph <doublecuph@gmail.com>
2025-04-22 17:14:32 +0300

    Script.js

--- Script.js
+++ Script.js
@@ -2,3 +2,11 @@ document.querySelector('.menu-toggle').addEventListener('click', function() {
     let menu = document.querySelector('.menu');
     menu.style.left = (menu.style.left === '-250px') ? '0px' : '-250px';
 });
+document.addEventListener('DOMContentLoaded', function() {
+    let adminMenu = document.getElementById('admin-menu');
+    
+    // Controllo se l'utente è admin
+    if (localStorage.getItem('adminLoggedIn') === 'true') {
+        adminMenu.style.display = 'block';
+    }
+});
